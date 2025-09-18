9b7843ee31e2e3a89370b9c4f9446a31743bd611
Sergio Martinez <mr.sergio1111@gmail.com>
2025-09-18 13:13:41 -0500

    Ajuste

--- Web/Movies/src/app/app.routes.ts
+++ Web/Movies/src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MoviesListComponent } from './movies-list/movies-list.component';
 
-export const routes: Routes = [];
+export const routes: Route[] = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'movies', component: MoviesListComponent },
+  { path: '**', redirectTo: '' }
+];
---
+++ Web/Movies/src/app/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],  standalone: true,
+  templateUrl: './home.html',
+  styleUrl: './home.scss'
+})
+export class Home {
+
+}
